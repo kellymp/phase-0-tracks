1284dfb7858d2059c6fc39a09d8d2bde529157a0
Michelle Kelly <michellepk89@gmail.com>
2016-04-12 17:08:50 -0500

    Add variables for answers

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,9 @@
 puts "What is your name?"
-name= gets.chomp
-puts "How old are you? What year were you born?"
-age= gets.chomp
+employee_name= gets.chomp
+puts "How old are you?"
+employee_age= gets.chomp
+puts= "What year were you born?"
+birth_year= gets.chomp
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 wants_garlic_bread= gets.chomp
 puts "Would you like to enroll in the company's health insurance? (y/n)"
