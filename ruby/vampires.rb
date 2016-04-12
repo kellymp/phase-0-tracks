c98abff72c1d6ac94870d4cf1a5a70653d62f240
Michelle Kelly <michellepk89@gmail.com>
2016-04-12 17:02:15 -0500

    Add gets.chomp to retrieve employee answers

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,10 @@
 puts "What is your name?"
 name= gets.chomp
 puts "How old are you? What year were you born?"
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-
-puts "Would you like to enroll in the company's health insurance?"
+age= gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+wants_garlic_bread= gets.chomp
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+wants_insurance= gets.chomp
 
 
