f9b7abdc90ae690902136c7fbc7487f4ee695fb7
Michelle Kelly <michellepk89@gmail.com>
2016-04-19 20:35:00 -0500

    add first portion of release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,11 @@
+# Release 2.1
+
+def build_array(first, second, third)
+	return [first, second, third]
+end
+
+# Release 1
+
 release_one = Array.new
 p release_one
 release_one << "hello" 
@@ -13,11 +21,14 @@ p release_one
 release_one.shift 
 p release_one
 if release_one.include?("horse")
-	puts "This array contains the word 'horse'."
+ puts "This array contains the word 'horse'."
 else 
-	puts "This array does not contain the word 'horse'."
+ puts "This array does not contain the word 'horse'."
 end
 release = ["word", 5, "cheese", true, nil]
 p release 
 join_releases = release + release_one
 p join_releases
+
+# Call method to return an array
+p build_array("word", 10, "bird")
