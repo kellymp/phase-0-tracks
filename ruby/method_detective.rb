52021557489d6bbf05deddd5296186dbe5baf9d3
Michelle Kelly <michellepk89@gmail.com>
2016-04-20 13:58:04 -0500

    add method to call methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
 
 # “zom”.<???>
 # => “zoom”
 
-"enhance".center(15)
+p "enhance".center(15)
 #=> “    enhance    ”
 
-"Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-"the usual".concat(" suspects")
+p "the usual".concat(" suspects")
 #=> “the usual suspects”
 
 #“ suspects”.<???>
 #=> “the usual suspects”
 
-"The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => “The case of the disappearing last lette”
 
-"The mystery of the missing first letter".slice(1..-1)
+p "The mystery of the missing first letter".slice(1..-1)
 # => “he mystery of the missing first letter”
 
 # “Elementary  ,    my   dear      Watson!”.<???>""
 # => “Elementary, my dear Watson!”
 
-"z".ord
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # 122 is the ordinal number for "z"
 
-"How many times does the letter ‘a’ appear in this string?".count "a"
+p "How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
