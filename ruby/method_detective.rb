865b405c6b967918f2249712b013e593d018d103
Michelle Kelly <michellepk89@gmail.com>
2016-04-20 13:55:39 -0500

    replace quotations in document to make strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,37 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-“iNvEsTiGaTiOn”.swapcase
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
 
 # “zom”.<???>
 # => “zoom”
 
-“enhance”.center(15)
+"enhance".center(15)
 #=> “    enhance    ”
 
-“Stop! You’re under arrest!”.upcase
+"Stop! You’re under arrest!".upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-“the usual”.concat(" suspects")
+"the usual".concat(" suspects")
 #=> “the usual suspects”
 
 #“ suspects”.<???>
 #=> “the usual suspects”
 
-“The case of the disappearing last letter”.chop
+"The case of the disappearing last letter".chop
 # => “The case of the disappearing last lette”
 
-“The mystery of the missing first letter”.slice(1..-1)
+"The mystery of the missing first letter".slice(1..-1)
 # => “he mystery of the missing first letter”
 
 # “Elementary  ,    my   dear      Watson!”.<???>""
 # => “Elementary, my dear Watson!”
 
-“z”.ord
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # 122 is the ordinal number for "z"
 
-“How many times does the letter ‘a’ appear in this string?”.count "a"
+"How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
