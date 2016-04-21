718f90718dd9e1a2fa53be4d234031f2f8df94d9
Michelle Kelly <michellepk89@gmail.com>
2016-04-20 21:37:47 -0500

    add overall hash for store and add women section hash with product hash and array of sales associate names

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,15 @@
+clothing_store = {
+	women_section: {
+		location: 'Front of Store',
+		product_info: {
+			shirts_quantity: 15,
+			pants_quantity: 20,
+			accessories_quantity: 5
+		}
+		Associate_name: [
+				"Katie",
+				"Susan"
+			]
+	}
+
+}
