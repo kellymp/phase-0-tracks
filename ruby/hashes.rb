a2a5561fee06434913eebf0a9cf0ba7b0587736c
Michelle Kelly <michellepk89@gmail.com>
2016-04-21 14:35:54 -0500

    pseudo code release 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+#create an empty hash for client data
+#prompt user for all data 
+#add all client data to the hash
+#print hash at bottom when all data in entered 
+#allow user to update any data, if user enters "none" skip to end
+#print most up-to-date hash 
+#exit program 
+
