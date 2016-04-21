a658edeabbb8b0ee82a216f0a7c2971105250c10
Michelle Kelly <michellepk89@gmail.com>
2016-04-21 15:18:06 -0500

    add prompts for client rooms, decor, and palette preference. convert necessary date to integers/booleans. add if statement to determine if palette preference is true or false

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,12 +2,22 @@
 client = {}
 #prompt user for data and add all client data to the hash
 puts "Enter the following client information:"
-puts "Client Name"
+puts "Client name:"
 client = {:name => gets.chomp}
-puts "Client Age"
-client [:age] = gets.chomp
-p client
+puts "Client age:"
+client [:age] = gets.chomp.to_i
+puts "Number of rooms in home:"
+client [:rooms] = gets.chomp.to_i
+puts "Decor theme:"
+client [:decor] = gets.chomp
+puts "Does client want a neutral palette?: (y/n)"
+if gets.chomp == "y"
+	client [:neutral_palette] = true
+else gets.chomp == "n"
+	client [:neutral_palette] = false
+end
 #print hash at bottom when all data in entered 
+p client
 #allow user to update any data, if user enters "none" skip to end
 #print most up-to-date hash 
 #exit program 
