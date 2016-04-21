c4745b91da76bfb60ef7207f305d4575872e727d
Michelle Kelly <michellepk89@gmail.com>
2016-04-20 22:11:17 -0500

    print 5 lines of nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,37 +1,43 @@
 clothing_store = {
-	womens_section: {
-		location: 'Front of Store',
-		product_info: {
-			shirts_quantity: 15,
-			pants_quantity: 20,
-			accessories_quantity: 5
-		}
-		Associate_name: [
-				'Katie',
-				'Susan'
-			]
-	}
-	mens_section: {
-		location: 'Middle of Store',
-		product_info: {
-			shirts_quantity: 10,
-			pants_quantity: 7,
-			accessories_quantity: 2
-		}
-		Associate_name: [
-				'Brian'
-				'Liz'
-			]
-	}
-	childrens_section: {
-			location: 'Back of Store',
-			product_info: {
-				shirts_quantity: 5,
-				pants_quantity: 5,
-				accessories_quantity: 5
-			}
-			Associate_name: [
-					'Rebecca',
-				]
-	}
+    womens_section: {
+        location: 'Front of Store',
+        product_info: {
+            shirts_quantity: 20,
+            pants_quantity: 15,
+            accessories_quantity: 12
+        },
+        associate_name: [
+            "Katie", 
+            "Susan"
+        ]
+    },
+    mens_section: {
+        location: 'Middle of Store',
+        product_info: {
+            shirts_quantity: 10,
+            seats_available: 7,
+            accessories_quantity: 2
+        },
+        associate_name: [
+            "Brian", 
+            "Liz"
+        ]
+    },
+    childrens_section: {
+        location: 'Back of Store',
+        product_info: {
+            shirts_quantity: 5,
+            pants_quantity: 5,
+            accessories_quantity: 4
+        },
+        associate_name: [
+        	"Marley"
+        	]
+    }
 }
+
+p clothing_store[:childrens_section][:associate_name]
+clothing_store[:mens_section][:associate_name].push("Kevin")
+p clothing_store[:mens_section][:associate_name]
+p clothing_store[:womens_section][:location]
+p clothing_store[:childrens_section][:location].upcase
