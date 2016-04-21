9ad7d42960a235f047611a7e2febcadb0113bb58
Michelle Kelly <michellepk89@gmail.com>
2016-04-21 14:48:29 -0500

    Add prompts for client name and age. Add age and name to client hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,12 @@
 #create an empty hash for client data
-#prompt user for all data 
-#add all client data to the hash
+client = {}
+#prompt user for data and add all client data to the hash
+puts "Enter the following client information:"
+puts "Client Name"
+client = {:name => gets.chomp}
+puts "Client Age"
+client [:age] = gets.chomp
+p client
 #print hash at bottom when all data in entered 
 #allow user to update any data, if user enters "none" skip to end
 #print most up-to-date hash 
