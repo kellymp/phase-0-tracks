6a0b5eaa8dfe0366f69b1586c2fd9c409a8e9507
Michelle Kelly <michellepk89@gmail.com>
2016-04-21 13:05:59 -0500

    Write psuedocode for release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+#release 0
+#split first name and last name into two different strings
+#swap first name and last name using reverse!
+#split the characters in first name and last name
+#create edge cases for a & u, b & z
+#change vowels to next in line using .next
+#change consonants to next in line using .next
+#join the letters back together to complete fake_name
