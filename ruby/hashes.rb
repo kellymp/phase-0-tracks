97237f4eeba01b8fcca0cdb67128baae3d3b6665
Michelle Kelly <michellepk89@gmail.com>
2016-04-22 18:06:57 -0500

    replace gets.chomp with variable in both if statements

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,54 +13,55 @@ client [:rooms] = gets.chomp.to_i
 puts "Decor theme:"
 client [:decor] = gets.chomp
 puts "Does client want a neutral palette?: (y/n)"
-if gets.chomp == "y"
-	client [:neutral_palette] = true
-else gets.chomp == "n"
-	client [:neutral_palette] = false
-end
+neutral= gets.chomp
+	if neutral == "y"
+		client [:neutral_palette] = true
+	else neutral == "n"
+		client [:neutral_palette] = false 
+	end
 puts "Estimated project length (in months):"
 client [:months_until_complete] = gets.chomp.to_i
 #print hash at bottom when all data in entered 
 p client
 #allow user to update any data, if user enters "none" skip to end
 puts "Please specify one of the following categories you would like to edit:"
-puts "Name" 
-puts "Age"
-puts "Address" 
-puts "Rooms" 
-puts "Decor"
-puts "Palette"
-puts "Project length"
+puts "name" 
+puts "age"
+puts "address" 
+puts "rooms" 
+puts "decor"
+puts "palette"
+puts "project length"
 puts "If all information is correct, enter 'none'."
-	if gets.chomp == "name"
+	revision = gets.chomp
+	if revision == "name"
 		puts "Client name:"
 		client = {:name => gets.chomp}
-	elsif gets.chomp == "age"
+	elsif revision == "age"
 		puts "Client age:"
 		client [:age] = gets.chomp.to_i
-	elsif gets.chomp == "address"
+	elsif revision == "address"
 		puts "Project address:"
 		client [:address] = gets.chomp
-	elsif gets.chomp == "rooms"
+	elsif revision == "rooms"
 		puts "Number of rooms in home:"
 		client [:rooms] = gets.chomp.to_i
-	elsif gets.chomp == "decor"
+	elsif revision == "decor"
 		puts "Decor theme:"
 		client [:decor] = gets.chomp	
-#	elsif gets.chomp == "palette"
-#		puts "Does client want a neutral palette?: (y/n)"
-#			if gets.chomp == "y"
-#			client [:neutral_palette] = true
-#			else gets.chomp == "n"
-#			client [:neutral_palette] = false
-#			end
-#	elsif gets.chpmp == "project length"
-#		puts "Estimated project length (in months):"
-#		client [:months_until_complete] = gets.chomp.to_i
-	else gets.chomp == "none"
+	elsif revision == "palette"
+		puts "Does client want a neutral palette?: (y/n)"
+			if gets.chomp == "y"
+			client [:neutral_palette] = true
+			else gets.chomp == "n"
+			client [:neutral_palette] = false
+			end
+	elsif revision == "project length"
+		puts "Estimated project length (in months):"
+		client [:months_until_complete] = gets.chomp.to_i
+	else revision == "none"
 		puts "No changes made."
 	end
 #print most up-to-date hash 
 p client
-#exit program 
-
+#exit program
