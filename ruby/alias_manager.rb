94a5a640f72131a2b29949c44f6d1d3568a0a1a7
Michelle Kelly <michellepk89@gmail.com>
2016-04-22 23:38:18 -0500

    add array to store fake spy aliases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,13 @@
 #Release 0
 #split first name and last name into two different strings
+#name.split(' ')
 #swap first name and last name using reverse!
+
 #split the characters in first name and last name
 #create edge cases for a & u, b & z
 #change vowels to next in line using .next
 #change consonants to next in line using .next
 #join the letters back together to complete fake_name
-
 
 
 #Release 1
@@ -18,4 +19,6 @@
 
 #Release 2
 #use array to store fake names enetered into interface
+spy_alias_database = []
+p spy_alias_database
 #print all data entered when user quits program
