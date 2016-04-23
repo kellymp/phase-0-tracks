59b224ef5f9228830529cdb162c327caad648df9
Michelle Kelly <michellepk89@gmail.com>
2016-04-22 23:42:19 -0500

    add interface that asks spy for their real name. Add until loop to loop program until user enters 'quit'

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,8 @@
 #Release 0
+#create a method to:
 #split first name and last name into two different strings
 #name.split(' ')
 #swap first name and last name using reverse!
-
 #split the characters in first name and last name
 #create edge cases for a & u, b & z
 #change vowels to next in line using .next
@@ -10,14 +10,18 @@
 #join the letters back together to complete fake_name
 
 
-#Release 1
+#Release 1 Interface
 #Ask for user's real name
+puts "What is your name?"
 #return fake name
+
 #create loop for interface until user enters "quit"
+until gets.chomp == "quit"
+	puts "what is your name?"
+end
 
 
-
-#Release 2
+#Release 2 Store aliases
 #use array to store fake names enetered into interface
 spy_alias_database = []
 p spy_alias_database
