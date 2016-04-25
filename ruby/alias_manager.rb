ff3e5503f69148a5e6a3bb2a872f4bf56375d85f
Michelle Kelly <michellepk89@gmail.com>
2016-04-24 22:30:45 -0500

    add method to advance to next vowel or consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,22 +13,35 @@ p split_name
 p name_swap
 
 #join swapped name together with space in between
-reverse_name = name_swap[0] + " " + name_swap[1]
-
+reverse_name = name_swap.join(' ')
 p reverse_name
 
+#split reversed name into individual characters
 reverse_characters = reverse_name.split('')
-
 p reverse_characters
 
-#split the characters in first name and last name
-#create edge cases for a & u, b & z
-#change vowels to next in line using .next
-#change consonants to next in line using .next
-#join the letters back together to complete fake_name
 
+#write method to advance vowel to next in alphabet and create edge cases for a & u
+def vowel_next(i)
+	vowels = "aAeEiIoOuU"
+	if i == "u"
+		i = "a"
+	elsif i == "U"
+		i = "A"
+	else 
+		i = vowels[vowels.index(i) +=2]
+end
 
-#Release 1 Interface
+#write method to advance consonants to next in alphabet and create edge cases for b & z
+def consonant_next(i)
+	consonants = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ"
+	if i == "z"
+		i = "b"
+	elsif i == "Z"
+		i == "B"
+	else
+		i = consonants[consonants.index(i) +=2]
+
 
 #return fake name
 #create loop for interface until user enters "quit"
