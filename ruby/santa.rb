b96771f32f4c78077a3f955ced2dbd9846a6825b
Michelle Kelly <michellepk89@gmail.com>
2016-04-28 15:58:16 -0500

    add diverse intializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,17 +16,30 @@ class Santa
 	end
 end
 
-santa = Santa.new
+santa = Santa.new(@gender, @ethnicity)
 santa.speak
 santa.eat_milk_and_cookies("sugar")
 santa.eat_milk_and_cookies("peppermint chocolate chip")
 
 #add empty array and add a lot of santa instances
 santas = []
-santas << Santa.new('agender', 'black')
-santas << Santa.new('female', 'Latino')
-santas << Santa.new('bigender', 'white')
-santas << Santa.new('male', 'Japanese')
-santas << Santa.new('female', 'prefer not to say')
-santas << Santa.new('gender fluid', 'Mystical Creature (unicorn)')
-santas << Santa.new('N/A', 'N/A')
+#santas << Santa.new('agender', 'black')
+#santas << Santa.new('female', 'Latino')
+#santas << Santa.new('bigender', 'white')
+#santas << Santa.new('male', 'Japanese')
+#santas << Santa.new('female', 'prefer not to say')
+#santas << Santa.new('gender fluid', 'Mystical Creature (unicorn)')
+#santas << Santa.new('N/A', 'N/A')
+
+#add some diverse initializations
+genders = ["agender", "female", "male", "bigender", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Chinese", "Mystical Creature (unicorn)", "pacific islander", "black"]
+
+genders.length.times do |i|
+	 puts "Creating a #{genders[i]} #{ethnicities[i]} Santa ..."
+	santas << Santa.new(genders[i], ethnicities[i])
+end
+
+
+
+
