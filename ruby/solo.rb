3021c59cb7fe235303197a4bac0af072780cdcbd
Michelle Kelly <michellepk89@gmail.com>
2016-04-28 21:05:35 -0500

    change attribute names

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,7 @@
 #create vespa class
-#create attributes: color, brand, motor
+#create attributes: color, name, motor size
 #create methods:
 # speed up method that takes an integer (speed) as its arguments
 # honk method that honks the number of times specified by the arguments
 # break method 
+
