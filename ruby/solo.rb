5ea0f4adc4645243052a3afb6c44ef8ef02b5cfd
Michelle Kelly <michellepk89@gmail.com>
2016-04-28 21:04:14 -0500

    add pseudo code for class with 3 attributes and 3 methods

---
+++ ruby/solo.rb
@@ -0,0 +1,6 @@
+#create vespa class
+#create attributes: color, brand, motor
+#create methods:
+# speed up method that takes an integer (speed) as its arguments
+# honk method that honks the number of times specified by the arguments
+# break method 
