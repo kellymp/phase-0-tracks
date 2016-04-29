c63327e199f8399a9260b4a3faa1ed0a5dd23aea
Michelle Kelly <michellepk89@gmail.com>
2016-04-28 21:22:05 -0500

    add driver code to test methods- doesnt work

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,36 @@
 #create vespa class
-#create attributes: color, name, motor size
+#create attributes: color, scooter name, motor size
 #create methods:
+# initailize  
 # speed up method that takes an integer (speed) as its arguments
 # honk method that honks the number of times specified by the arguments
-# break method 
+# slam on the breaks method 
 
+class Vespa
+
+#initialize class and add attributes
+	def initialize(color,scooter_name)
+		@color = color
+		@name = scooter_name
+		@motor_size = motor_size
+	end
+
+#method for speeding up the vespa
+	def speed_up(speed)
+		puts "Vrooom! The Vespa is now going #{speed}mph!"
+	end
+
+#method for honking the horn
+	def honk(times)
+		puts "Honk! " * times
+	end
+
+#method for slamming on the breaks
+	def slam_breaks
+		puts "SQEEEEEEEEEEEE! The Vespa has stopped!"
+	end
+end
+
+p Vespa.speed_up(40)
+p Vespa.honk(5)
+p Vespa.slam_breaks
