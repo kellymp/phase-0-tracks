15450787f0a56baab722be7ec668ff3ae7ca3fbb
Michelle Kelly <michellepk89@gmail.com>
2016-04-30 22:44:05 -0500

    comment out code on line 3 and 7 previously missed. add module for shout. add class for positive_emotion and negative_emotion. add mixin of shout to both classes. add driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,32 @@
 #module Shout
 #	def self.yell_angrily(words)
-		words + "!!!" + ":("
+#		words + "!!!" + ":("
 #	end
 
 #	def self.yelling_happily(words)
-		words + "!!!!!!" + " :)"
+#		words + "!!!!!!" + " :)"
 #	end
 #end
 
 #p Shout.yelling_happily("HELLOOOOOOO WOOOOORRRRLLLLD")
 #p Shout.yell_angrily("I am very tired")
 
+module Shout
+	def yell(words)
+		puts "I FEEL SO #{words}!!!!"
+	end
+end
+
+class Positive_emotion
+	include Shout
+end
+
+class Negative_emotion
+	include Shout
+end
+
+positive_emotion = Positive_emotion.new
+positive_emotion.yell("ALIVE")
+
+negative_emotion = Negative_emotion.new
+negative_emotion.yell("DEAD INSIDE")
