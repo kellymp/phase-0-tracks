bb31bc420a71062b1872bb78a811808d03b42c56
Michelle Kelly <michellepk89@gmail.com>
2016-05-03 16:02:51 -0500

    create explore javascript file and add pseudo code for a method that reverses a string

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// create a method that takes a string as parameters 
+	//split the string to prepare it to be reversed
+	//reverse the split string
+	//rejoin the string together
+	//print the reversed sting to the console
+
