6fcbd0ebbcfb87dbf15e8d34834dfa0879c2f4a0
Michelle Kelly <michellepk89@gmail.com>
2016-05-03 16:13:18 -0500

    initial attempt to reverse a string. JavaScript does not have built in ruby method so it did not work

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,15 @@
-// create a method that takes a string as parameters 
+//reverse a string
+	// create a method that takes a string as parameters 
 	//split the string to prepare it to be reversed
 	//reverse the split string
 	//rejoin the string together
 	//print the reversed sting to the console
 
+function reverseString(string) {
+	string.split('');
+	string.reverse();
+	string.join('');
+	return string;
+	}
+
+reverseString("michelle")
