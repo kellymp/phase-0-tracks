88afad43e333bd911e721ddbdce90224609969ed
Michelle Kelly <michellepk89@gmail.com>
2016-05-05 15:26:55 -0500

    comment out reverseString method that uses built in methods

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,6 @@
+
+
+// The below method uses built in methods
 //create a method to reverse a string
 	//split string turning it into an array 
 	//reverse string array
@@ -5,17 +8,17 @@
 	//print the string backwards
 
 //define method that takes a string as a parameter
-function reverseString(string) {
+//function reverseString(string) {
 	// set a variable equal to the string split, reversed, and joined back together
-	var stringReversed = string.split('').reverse().join('');
+//	var stringReversed = string.split('').reverse().join('');
 
 	//print the variable if the specidied condition is true using an if statement
-	if (stringReversed == stringReversed) { 
-		return stringReversed;
-	}
-}
+//	if (stringReversed == stringReversed) { 
+//		return stringReversed;
+//	}
+//}
 
-console.log(reverseString("michelle"));
-console.log(reverseString("michelle kelly"));
-console.log(reverseString("coding is fun"));
+//console.log(reverseString("michelle"));
+//console.log(reverseString("michelle kelly"));
+//console.log(reverseString("coding is fun"));
 
