19f37eae5ea112e37538c3f14678792e0af682fd
Michelle Kelly <michellepk89@gmail.com>
2016-05-12 13:10:10 -0500

    add additional methods and bang operator versions of methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -6,6 +6,10 @@
 
 p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
+#or
+p "iNvEsTiGaTiOn".swapcase! #bang operator version
+# => “InVeStIgAtIoN”
+
 
 p "zom".insert(2, "o")
 # => “zoom”
@@ -13,31 +17,52 @@ p "zom".insert(2, "o")
 p "zom".replace "zoom"
 # => “zoom”
 
+
 p "enhance".center(15)
 #=> “    enhance    ”
 
+
 p "Stop! You’re under arrest!".upcase
 # => “STOP! YOU’RE UNDER ARREST!”
+#or
+p "Stop! You’re under arrest!".upcase! #bang operator version
+# => “STOP! YOU’RE UNDER ARREST!”
+
 
 p "the usual".concat(" suspects")
 #=> “the usual suspects”
+#or
+p "the usual".insert(-1," suspects")
+#=> “the usual suspects”
+
 
 p " suspects".prepend("the usual")
 #=> “the usual suspects”
+#or
+p " suspects".insert(0,"the usual")
+#=> “the usual suspects”
+
 
 p "The case of the disappearing last letter".chop
 # => “The case of the disappearing last lette”
 
+
 p "The mystery of the missing first letter".slice(1..-1)
 # => “he mystery of the missing first letter”
 
+
 p "Elementary  ,    my   dear      Watson!".squeeze
 # => “Elementary, my dear Watson!”
+#or
+p "Elementary  ,    my   dear      Watson!".squeeze! #bang operator version
+# => “Elementary, my dear Watson!”
+
 
 p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # 122 is the ordinal number for "z"
+
 
 p "How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
