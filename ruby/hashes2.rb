b34e294448d98eb8f19a3e6c08a3d28b468cacee
Michelle Kelly <michellepk89@gmail.com>
2016-05-12 13:43:16 -0500

    pseudocode release 3

---
+++ ruby/hashes2.rb
@@ -0,0 +1,19 @@
+#create an empty array to store client data
+
+#prompt user for the following data: 
+#name (string)
+#address (string)
+#number of rooms (integer)
+#decor style (string)
+#palette (boolean: true or false for neutral palette)
+#likes color (boolean)
+#project length (integer)
+
+#convert user input to appropriate data type using .to_i .to_s 
+
+#print the hash with user data 
+
+#use if statement to give user the opportunity to update any key/value pairs
+
+#print the updated hash and exit program
+
