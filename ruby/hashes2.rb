7da8f62f7009fd0634139dae90c8f11eaadc5c57
Michelle Kelly <michellepk89@gmail.com>
2016-05-12 14:07:50 -0500

    add if statement for neutral_palette to assign boolean value

--- ruby/hashes2.rb
+++ ruby/hashes2.rb
@@ -4,26 +4,37 @@ client = {}
 puts "Michelle Kelly Interiors Client information"
 puts "-------------------------------------------"
 puts "Enter the following client information:"
+
 #name (string)
 puts "Client Name:"
-client[:name] = gets.chomp
+client[:name] = gets.chomp.to_s
 # or client = {:name => gets.chomp}
+
 #address (string)
 puts "Project Address:"
-client[:address] = gets.chomp
+client[:address] = gets.chomp.to_s
+
 #number of rooms (integer)
 puts "Number of Rooms to Design:"
-client[:rooms] = gets.chomp
-#aesthetic (string)
-puts "Client's Design aesthetic"
-client[:aesthetic] = gets.chomp
-#palette (boolean: true or false for neutral palette)
-puts "Client Wants Neutral Palette (true/false)"
-client[:palette] = gets.chomp
-#project length (integer)
-puts "Estimated Project Length (in months)"
-client[:length] = gets.chomp
+client[:rooms] = gets.chomp.to_i
 
+#aesthetic (string)
+puts "Client's Design aesthetic:"
+client[:aesthetic] = gets.chomp.to_s
+
+#neutral_palette (boolean: true or false for neutral palette)
+puts "Client Wants Neutral Palette?: (yes/no)"
+preference = gets.chomp
+	#Use if statement to set neutral_palette to true or false
+	if preference == "yes"
+		client[:neutral_palette] = true
+	else neutral == "no"
+		client[:neutral_palette] = false
+	end
+
+#project length (integer)
+puts "Estimated Project Length (in months):"
+client[:length] = gets.chomp.to_i
 #convert user input to appropriate data type using .to_i .to_s 
 
 #print the hash with user data 
