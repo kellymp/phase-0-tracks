86b0a61747b6e1f88e5fa00952a9438a2592e27e
Michelle Kelly <michellepk89@gmail.com>
2016-05-12 13:58:32 -0500

    add interface for user to enter client data. Add user responses to hash

--- ruby/hashes2.rb
+++ ruby/hashes2.rb
@@ -1,13 +1,28 @@
 #create an empty array to store client data
 client = {}
 #prompt user for the following data: 
+puts "Michelle Kelly Interiors Client information"
+puts "-------------------------------------------"
+puts "Enter the following client information:"
 #name (string)
+puts "Client Name:"
+client[:name] = gets.chomp
+# or client = {:name => gets.chomp}
 #address (string)
+puts "Project Address:"
+client[:address] = gets.chomp
 #number of rooms (integer)
-#decor style (string)
+puts "Number of Rooms to Design:"
+client[:rooms] = gets.chomp
+#aesthetic (string)
+puts "Client's Design aesthetic"
+client[:aesthetic] = gets.chomp
 #palette (boolean: true or false for neutral palette)
-#likes color (boolean)
+puts "Client Wants Neutral Palette (true/false)"
+client[:palette] = gets.chomp
 #project length (integer)
+puts "Estimated Project Length (in months)"
+client[:length] = gets.chomp
 
 #convert user input to appropriate data type using .to_i .to_s 
 
