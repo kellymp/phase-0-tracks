4c7e890f747fe829afc944917d01f4be74b2bd9e
Michelle Kelly <michellepk89@gmail.com>
2016-05-15 18:48:43 -0500

    add steps 4-6

--- ruby/arrays_drill_2.rb
+++ ruby/arrays_drill_2.rb
@@ -11,3 +11,19 @@ p array_one
 array_one.delete_at(2)
 p array_one
 
+#insert a new item at index 2 and print the array
+array_one.insert(2, true)
+p array_one
+
+#remove the first item of the array without having to refer to its index
+array_one.shift
+p array_one
+
+#ask the array if it includes a certain item and print results of this call method
+if array_one.include?('cheeseburger')
+	puts "This array includes the word 'cheeseburger'!"
+else
+	puts "This array does not include the word 'cheeseburger'!"
+end
+
+
