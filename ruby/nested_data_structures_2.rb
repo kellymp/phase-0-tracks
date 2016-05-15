4913a1124d2069fc4c382d02d07faed94fdbd918
Michelle Kelly <michellepk89@gmail.com>
2016-05-15 17:04:09 -0500

    add inventory hash to each grocery category hash

--- ruby/nested_data_structures_2.rb
+++ ruby/nested_data_structures_2.rb
@@ -1,13 +1,25 @@
 grocery_store = {
 	produce: {
 		location: 'front of store',
+		inventory: {
+			tomatoes: 20,
+			lettuce_heads: 25,
 		},
+	},
 
 	dairy: {
 		location: 'middle of store',
+		inventory: {
+			milk_cartons: 30,
+			cream_cheese_tubs: 20,
+		},
 	},
 
 	meat: {
 		location: 'back of store',
+		inventory: {
+			hamburger_patties: 10,
+			steaks: 15,
+		},
 	}
 }
