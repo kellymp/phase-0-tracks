b1d208e04e19ea06374b1fe3e290c51f408179b5
Michelle Kelly <michellepk89@gmail.com>
2016-05-15 17:17:53 -0500

    add arrays for restock lists

--- ruby/nested_data_structures_2.rb
+++ ruby/nested_data_structures_2.rb
@@ -1,18 +1,30 @@
 grocery_store = {
 	produce: {
 		location: 'front of store',
+
 		inventory: {
 			tomatoes: 20,
 			lettuce_heads: 25,
 		},
+
+		needs_restocking: [
+			'lemons',
+			'ears of corn'
+		]
 	},
 
 	dairy: {
 		location: 'middle of store',
+
 		inventory: {
 			milk_cartons: 30,
 			cream_cheese_tubs: 20,
 		},
+
+		needs_restocking: [
+			'sour cream',
+			'string cheese'
+		]
 	},
 
 	meat: {
@@ -21,5 +33,10 @@ grocery_store = {
 			hamburger_patties: 10,
 			steaks: 15,
 		},
+
+		needs_restocking: [
+			'filet mignon',
+			'beef tenderloin'
+		]
 	}
 }
