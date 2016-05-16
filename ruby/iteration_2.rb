81736e4e8b75871653485aeb70ec05312285f786
Michelle Kelly <michellepk89@gmail.com>
2016-05-15 23:01:30 -0500

    add release 2

--- ruby/iteration_2.rb
+++ ruby/iteration_2.rb
@@ -55,3 +55,52 @@ end
 
 puts "Array after using .map!:"
 p sports
+
+
+
+#Release 2
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+test_scores = {"john" => 45, "mary" => 75, "chris" => 90, "sam" => 30, "jim" => 20}
+
+# 1. delete any even numbers from the array
+arr.delete_if {|num| num % 2 == 0}
+p arr
+
+# deletes all people who scored less than a 50
+test_scores.delete_if {|name, grade| grade < 50}
+p test_scores
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+test_scores = {"john" => 45, "mary" => 75, "chris" => 90, "sam" => 30, "jim" => 20}
+
+#2 keep all even numbers
+arr.keep_if {|num| num % 2 == 0}
+p arr
+#keep if test scores are higher or equal to 50
+test_scores.keep_if {|name, grade| grade >= 50}
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+test_scores = {"john" => 45, "mary" => 75, "chris" => 90, "sam" => 30,  "jim" => 20}
+
+#selects numbers greater than 6
+arr. select!{|num| num > 6}
+p arr
+
+#selects names that start with j
+test_scores.select!{|name, grade| name[0] == "j"}
+p test_scores
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+test_scores = {"john" => 45, "mary" => 75, "chris" => 90, "sam" => 30,  "jim" => 20}
+
+#removes items until it finds a number that is not less than 6, then stops
+arr = arr.drop_while{|num| num < 6}
+p arr
+
+#removes items until it finds a grade greater than 80, then stops, which turns hash into an array
+test_scores = test_scores.drop_while{|name,grade| grade < 80}
+
+#switch test scores back to a hash
+test_scores = test_scores.to_h
+p test_scores
