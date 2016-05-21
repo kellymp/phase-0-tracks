e82e71a047f96375053516a65d13eb1f8c7c479a
Michelle Kelly <michellepk89@gmail.com>
2016-05-21 13:49:49 -0500

    add attributes to santa class in initialize

--- ruby/santa_2.rb
+++ ruby/santa_2.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
 	def speak
