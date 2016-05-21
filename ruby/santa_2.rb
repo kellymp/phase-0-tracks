035b251fb79fadb047f8aec9c52a906066d3846d
Michelle Kelly <michellepk89@gmail.com>
2016-05-21 14:47:34 -0500

    add driver code and ethnicity and gender arrays

--- ruby/santa_2.rb
+++ ruby/santa_2.rb
@@ -23,3 +23,16 @@ santa = Santa.new
 santa.speak
 santa.eat_milk_and_cookies('sugar')
 
+#santas = []
+#santas << Santa.new('female', 'black')
+#santas << Santa.new('male', 'asian')
+
+santas = []
+example_genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fluid', 'N/A']
+example_ethnicities = ['black', 'latino', 'white', 'Japanese-African', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
+
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+	
