9a7cfdd1b60572aa98889d38decd711224901433
Michelle Kelly <michellepk89@gmail.com>
2016-05-21 11:30:22 -0500

    add additional methods and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,9 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(times)
+  	puts 'Woof' * times
+  end
+
+  def roll_over
+  	puts '*rolls over*'
+  end
+
+  def dog_years(human_years)
+  	p human_years.to_i * 7
+  end
+
+  def shake
+  	puts '*puts paw out to shake*'
+  end
+
 end
 
 Lola = Puppy.new
 Lola.fetch('ball')
+Lola.speak(6)
+Lola.roll_over
+Lola.dog_years(5)
+Lola.shake
+
 
 
