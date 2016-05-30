ec19ee8128f4ab3876094831ed7b7551e3cecddc
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 15:05:16 -0500

    add pseudocoding for method that reverses a string

---
+++ js/explore_2.js
@@ -0,0 +1,4 @@
+//create a method to reverse a string
+	//define a method that takes a string as the parameters
+	//use for loop to print the string backwards .length and string's index
+	//assign a new index to each letter of the string to print reversed 
