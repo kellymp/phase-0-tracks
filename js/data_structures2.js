e86d579a14d37b6b05b2f71d1e5fdaf9e529b3ea
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 16:45:23 -0500

    add driver code to release 0. Add release 1

--- js/data_structures2.js
+++ js/data_structures2.js
@@ -1,7 +1,20 @@
 //declare two arrays for colors and names
 var colors = ['blue', 'green', 'yellow', 'orange'];
-var names = ['Finnigan', 'Black Beauty', 'Bailey', 'Cinnamon'];
+var names = ['Finnigan', 'Camille', 'Bailey', 'Cinnamon'];
 
 //add another color and name to the corresponding arrays
 colors.push('red');
 names.push('Nutella');
+
+console.log(colors)
+console.log(names)
+
+//assign colors to horse names using an object
+function assignColors(names, colors) {
+	var result = {};
+	for (var i = 0; i < names.length; i++)
+			result[names[i]] = colors[i];
+		return result
+}
+
+console.log(assignColors(names, colors));
