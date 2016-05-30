24a07bc3eed9e4f81ce2b53d1dffbafda4cadf31
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 15:11:46 -0500

    change method to function in pseudocode

--- js/explore_2.js
+++ js/explore_2.js
@@ -1,5 +1,5 @@
-//create a method to reverse a string
-	//define a method that takes a string as the parameters
+//create a function to reverse a string
+	//define a function that takes a string as the parameters
 	//use for loop to print the string backwards .length and string's index
 	//assign a new index to each letter of the string to print reversed 
 
@@ -19,3 +19,4 @@ console.log(reverseString("it's finally summer"));
 
 //add driver code that store the results of the function in a variable 
 var results = reverseString('hello world!')
+
