d7046799fe311cb694f1db8ef2fc46a630cbe930
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 16:39:36 -0500

    add release 0

---
+++ js/data_structures2.js
@@ -0,0 +1,7 @@
+//declare two arrays for colors and names
+var colors = ['blue', 'green', 'yellow', 'orange'];
+var names = ['Finnigan', 'Black Beauty', 'Bailey', 'Cinnamon'];
+
+//add another color and name to the corresponding arrays
+colors.push('red');
+names.push('Nutella');
