0ef3b24f57b0fcf52547988f8abc96c7156055f0
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 15:09:25 -0500

    add method reverse a string and driver code

--- js/explore_2.js
+++ js/explore_2.js
@@ -2,3 +2,17 @@
 	//define a method that takes a string as the parameters
 	//use for loop to print the string backwards .length and string's index
 	//assign a new index to each letter of the string to print reversed 
+
+function reverseString(string) {
+	var stringReversed = '';
+	//for loop to reverse 
+	for (var i = string.length -1; i >= 0; i--)
+		//move the charcter at an index to the end and set stringReversed equal to this new string
+		stringReversed += string[i];
+	return stringReversed;
+}
+
+//driver code
+console.log(reverseString("michelle"));
+console.log(reverseString("michelle kelly"));
+console.log(reverseString("it's finally summer"));
