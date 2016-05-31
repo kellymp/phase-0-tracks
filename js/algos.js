01afed47840fc061eeab5d2c785c61170c1c1665
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 22:43:39 -0500

    Comment out function- not working

--- js/algos.js
+++ js/algos.js
@@ -16,13 +16,22 @@ function longestPhraseFinder(array) {
 
 //release 1
 //create a function that compares two objects to check for shared key-value pairs
+//set variables to the array of each object
 //iterate through both objects to check if the values match
 //use if conditional to return true if the values match, return false if they do not
 
+//function compareObjects(object1, object2) {
+//	var keyOne = Object.keys(object1);
+//	var keyTwo = Object.keys(object2);
+//	var match = "false"
 
-
-
-
+//	for (var i = 0; i < objOne.length; i++) {
+//		if (objOne[i] == objTwo[i]) {
+//			return true;
+//		}
+//	}
+//	return false;
+//}
 
 
 //driver code for phase 0
