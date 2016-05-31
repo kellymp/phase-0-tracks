294f5baafb4472a366c87f3807be69bdcfea7962
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 22:14:29 -0500

    pseudocode phase 1

--- js/algos.js
+++ js/algos.js
@@ -14,7 +14,18 @@ function longestPhraseFinder(array) {
     console.log(longestPhrase);    	
 }
 
-//driver code
+//release 1
+//create a function that compares two objects to check for shared key-value pairs
+//iterate through both objects to check if the values match
+//use if conditional to return true if the values match, return false if they do not
+
+
+
+
+
+
+
+//driver code for phase 0
 longestPhraseFinder(["Michelle", "hello world", "heeeeeeeey", "its summertime", "yes"])
 longestPhraseFinder(['January', 'March', 'June', 'November'])
 longestPhraseFinder(['blue', 'orange', 'magenta', 'citrus yellow', 'red'])
