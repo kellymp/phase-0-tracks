956a8adbf855e012e3f1906eb231c4cc1c1d0abe
Michelle Kelly <michellepk89@gmail.com>
2016-05-30 23:00:45 -0500

    complete release 1 and add driver code

--- js/algos.js
+++ js/algos.js
@@ -16,25 +16,33 @@ function longestPhraseFinder(array) {
 
 //release 1
 //create a function that compares two objects to check for shared key-value pairs
-//set variables to the array of each object
 //iterate through both objects to check if the values match
 //use if conditional to return true if the values match, return false if they do not
 
-//function compareObjects(object1, object2) {
-//	var keyOne = Object.keys(object1);
-//	var keyTwo = Object.keys(object2);
-//	var match = "false"
+function compareObjects(object1, object2) {
+	var value;
 
-//	for (var i = 0; i < objOne.length; i++) {
-//		if (objOne[i] == objTwo[i]) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
+	for (value in object1) {
+		valueOne = object1[value];
+	for (value in object2) {
+		valueTwo = object2[value];
+			if (valueOne == valueTwo) {
+				return true;
+			}
+			else {
+			}
+	}
+	}
+ 			return false
+}
 
 
 //driver code for phase 0
 longestPhraseFinder(["Michelle", "hello world", "heeeeeeeey", "its summertime", "yes"])
 longestPhraseFinder(['January', 'March', 'June', 'November'])
 longestPhraseFinder(['blue', 'orange', 'magenta', 'citrus yellow', 'red'])
+
+//Driver code release 1
+console.log(compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54}))
+console.log(compareObjects({name: "Steven", age: 35}, {name: "Tamir", age: 54}))
+console.log(compareObjects({name: "Steven", age: 30}, {name: "Steven", age: 54}))
