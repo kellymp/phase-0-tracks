4c070200f428fa705a2fcb56527f319fbf8f94b0
Michelle Kelly <michellepk89@gmail.com>
2016-06-06 16:28:18 -0500

    add test values to orders. comment out driver code that wont work

--- databases/Michelles_Market/market.rb
+++ databases/Michelles_Market/market.rb
@@ -7,16 +7,16 @@ db = SQLite3::Database.new("market_logs.db")
 
 #table for customers
 create_customers = <<-SQL
-  CREATE TABLE IF NOT EXISTS customers(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    phone INTEGER,
-    address VARCHAR(255),
-    city VARCHAR (255),
-    state VARCHAR (255),
-    zip INTEGER
-  )
+ CREATE TABLE IF NOT EXISTS customers(
+   id INTEGER PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255),
+   phone INTEGER,
+   address VARCHAR(255),
+   city VARCHAR (255),
+   state VARCHAR (255),
+   zip INTEGER
+ )
 SQL
 
 #table for orders
@@ -25,9 +25,11 @@ create_orders = <<-SQL
     id INTEGER PRIMARY KEY,
     item VARCHAR(255),
     quantity INTEGER,
+    customer INTEGER,
     shipped BOOLEAN
   )
 SQL
+
 
 #create customers table
 db.execute(create_customers)
@@ -38,14 +40,17 @@ db.execute(create_orders)
 #add test customer
 #db.execute("INSERT INTO customers (name, email, phone, address, city, state, zip) VALUES ('Simone Rockwell', 'simrock@gmail.com', 8476665783, '450 Canteburry Cove St.', 'Lake George', 'PA', 67995)")
 
+#add test order
+#db.execute("INSERT INTO orders (item, quantity, customer, shipped) VALUES ('white sneakers', 1, 5, 'false')")
+
 #create method to add multiple customers to the customers table
 def add_customers(db, name, email, phone, address, city, state, zip)
 	db.execute("INSERT INTO customers (name, email, phone, address, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?)", [name, email, phone, address, city, state, zip])
 end
 
-#create method to add multiple order to the orders table
-def add_orders(db, item, quantity, shipped)
-	db.execute("INSERT INTO orders (item, quantity, shipped) VALUES (?, ?, ?)", [item, quantity, shipped])
+#create method to add multiple orders to the orders table
+def add_orders(db, item, quantity, customer, shipped)
+	db.execute("INSERT INTO orders (item, quantity, customer, shipped) VALUES (?, ?, ?, ?)", [item, quantity, customer, shipped])
 end
 
 #add values to customers table
@@ -53,7 +58,11 @@ end
 #	add_customers(db, Faker::Name.name, Faker::Internet.email, Faker::PhoneNumber.phone_number, Faker::Address.street_address, Faker::Address.city, Faker::Address.state_abbr, Faker::Address.zip_code)
 #end
 
+#Note:cannot get this driver code to work- not sure why as I did it exactly the same as the above code that works
 #add values to orders table
+#40.times do 
+#	add_orders(db, Faker::Commerce.product_name, Faker::Number.between(1, 10), Faker::Number.between(1, 30), Faker::Boolean.boolean(0.7))
+#end
 
 
 
