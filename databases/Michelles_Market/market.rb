041070184e9137bec8a05bdb1af6d3bb1c36a188
Michelle Kelly <michellepk89@gmail.com>
2016-06-06 15:36:03 -0500

    add orders table to database

--- databases/Michelles_Market/market.rb
+++ databases/Michelles_Market/market.rb
@@ -5,7 +5,7 @@ require 'faker'
 # create SQLite3 database
 db = SQLite3::Database.new("market_logs.db")
 
-#table for customers, orders, and payment info
+#table for customers
 create_customers = <<-SQL
   CREATE TABLE IF NOT EXISTS customers(
     id INTEGER PRIMARY KEY,
@@ -19,8 +19,21 @@ create_customers = <<-SQL
   )
 SQL
 
+#table for orders
+create_orders = <<-SQL
+  CREATE TABLE IF NOT EXISTS orders(
+    id INTEGER PRIMARY KEY,
+    item VARCHAR(255),
+    quantity INTEGER,
+    shipped BOOLEAN
+  )
+SQL
+
 #create customers table
 db.execute(create_customers)
+
+#create orders table
+db.execute(create_orders)
 
 #add test customer
 #db.execute("INSERT INTO customers (name, email, phone, address, city, state, zip) VALUES ('Simone Rockwell', 'simrock@gmail.com', 8476665783, '450 Canteburry Cove St.', 'Lake George', 'PA', 67995)")
@@ -30,6 +43,17 @@ def add_customers(db, name, email, phone, address, city, state, zip)
 	db.execute("INSERT INTO customers (name, email, phone, address, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?)", [name, email, phone, address, city, state, zip])
 end
 
-30.times do 
-	add_customers(db, Faker::Name.name, Faker::Internet.email, Faker::PhoneNumber.phone_number, Faker::Address.street_address, Faker::Address.city, Faker::Address.state_abbr, Faker::Address.zip_code)
+#create method to add multiple order to the orders table
+def add_orders(db, item, quantity, shipped)
+	db.execute("INSERT INTO orders (item, quantity, shipped) VALUES (?, ?, ?)", [item, quantity, shipped])
 end
+
+#add values to customers table
+#30.times do 
+#	add_customers(db, Faker::Name.name, Faker::Internet.email, Faker::PhoneNumber.phone_number, Faker::Address.street_address, Faker::Address.city, Faker::Address.state_abbr, Faker::Address.zip_code)
+#end
+
+#add values to orders table
+
+
+
