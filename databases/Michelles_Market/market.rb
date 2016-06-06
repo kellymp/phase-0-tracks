3d3c348c74f1968100f070355eb40d67f7bf309e
Michelle Kelly <michellepk89@gmail.com>
2016-06-06 15:05:23 -0500

    add customer table and test customer values

--- databases/Michelles_Market/market.rb
+++ databases/Michelles_Market/market.rb
@@ -5,3 +5,22 @@ require 'faker'
 # create SQLite3 database
 db = SQLite3::Database.new("market_logs.db")
 
+#table for customers, orders, and payment info
+create_customers = <<-SQL
+  CREATE TABLE IF NOT EXISTS customers(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    phone INTEGER,
+    address VARCHAR(255),
+    city VARCHAR (255),
+    state VARCHAR (255),
+    zip INTEGER
+  )
+SQL
+
+#create customer table
+db.execute(create_customers)
+
+#add test customer
+#db.execute("INSERT INTO customers (name, email, phone, address, city, state, zip) VALUES ('Simone Rockwell', 'simrock@gmail.com', 8476665783, '450 Canteburry Cove St.', 'Lake George', 'PA', 67995)")
