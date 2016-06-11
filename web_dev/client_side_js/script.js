20e8c002215d24389d895726d209a43147a91ff9
Michelle Kelly <michellepk89@gmail.com>
2016-06-11 16:28:32 -0500

    remove emphasis border

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,7 +1,5 @@
-var emph = document.getElementsByTagName("em")
-emph.style.border = "3px solid blue"
-
-var section_headers = document.getElementsByTagName("h2")
-section_headers[0].style.border = "black solid 2px"
-section_headers[1].style.color = "#000099"
-section_headers[2].style.fontFamily = "helvetica"
+var sectionHeaders = document.getElementsByTagName("h2");
+sectionHeaders[0].style.border = "black solid 2px";
+sectionHeaders[1].style.color = "#000099";
+sectionHeaders[2].style.fontFamily = "helvetica";
+sectionHeaders[2].style.color = "red";
