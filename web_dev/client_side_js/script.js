710a57832ac468ea74a49910bc6d9ea80ca8d3b0
Michelle Kelly <michellepk89@gmail.com>
2016-06-11 16:19:01 -0500

    add content to index. add style to script

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,7 @@
+var emph = document.getElementsByTagName("em")
+emph.style.border = "3px solid blue"
+
+var section_headers = document.getElementsByTagName("h2")
+section_headers[0].style.border = "black solid 2px"
+section_headers[1].style.color = "#000099"
+section_headers[2].style.fontFamily = "helvetica"
